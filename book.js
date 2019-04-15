95e93a0676db818ba032d4bbeefac288c9896a39
John Nguyen <john.nguyen3@seattlecentral.edu>
2019-04-15 15:51:37 +0000

    bugsweep

--- book.js
+++ book.js
@@ -1,5 +1,6 @@
 var books = [
-    { title:"Dune", author:"Frank Herbert", pubdate:1965 }
+    //var person = {firstName:"John", lastName:"Doe", age:46};
+    { title:"Dune", author:"Frank Herbert", pubdate:1965 },
     { title:"Moby Dick", author:"Herman Melville", pubdate:1851 },
     { title:"The Adventures of Tom Sawyer", author:"Mark Twain", pubdate:1876 },
     { title:"War and Peace", author:"Leo Tolstoy", pubdate:1869 },
