235ea0f24a81f25f8174864408982e2159d95218
John Nguyen <john.nguyen3@seattlecentral.edu>
2019-04-15 15:50:29 +0000

    filled with books

---
+++ book.js
@@ -0,0 +1,7 @@
+var books = [
+    { title:"Dune", author:"Frank Herbert", pubdate:1965 }
+    { title:"Moby Dick", author:"Herman Melville", pubdate:1851 },
+    { title:"The Adventures of Tom Sawyer", author:"Mark Twain", pubdate:1876 },
+    { title:"War and Peace", author:"Leo Tolstoy", pubdate:1869 },
+    { title:"The Great Gatsby", author:"F. Scott Fitzgerald", pubdate:1925 },
+];
