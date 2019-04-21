9c4b09b36035530be23439e605286d0b2f72d690
John Nguyen <john.nguyen3@seattlecentral.edu>
2019-04-21 12:12:30 +0000

    cleaned up book.js

--- book.js
+++ book.js
@@ -1,5 +1,4 @@
 var books = [
-    //var person = {firstName:"John", lastName:"Doe", age:46};
     { title:"Dune", author:"Frank Herbert", pubdate:1965 },
     { title:"Moby Dick", author:"Herman Melville", pubdate:1851 },
     { title:"The Adventures of Tom Sawyer", author:"Mark Twain", pubdate:1876 },
@@ -7,22 +6,12 @@ var books = [
     { title:"The Great Gatsby", author:"F. Scott Fitzgerald", pubdate:1925 },
 ];
 
+//simply returns the array, not any copies or transforms
 exports.getAll = function() {
-    // books.map( function(item) {
-    //     return { title : item.title, author : item.author, pubdate : item.pubdate }
-    // });
-    //return copy of array books.slice()
-    //return the array
     return books;
 };
 
-//exports.myHandler = function(event, context, callback) {   
-    //... function code   
-    //callback(null, "some success message");
-   // or 
-   // callback("some error type"); 
-//}
-
+//returns request book element where element's title matches key
 exports.get = function(key) {
     var myFunction = function(book) {
         return book.title === key;
@@ -30,27 +19,13 @@ exports.get = function(key) {
     return books.find(myFunction, key);
 };
 
-/*
-let foundIndex = students.findIndex((student) => {
-    return student.name === 'mary';
-});
-*/
-
-//use methods findIndex() and splice() to locate and delete array element
+//uses methods findIndex() and splice() to locate and delete array element
 exports.delete = function(key) {
     //locate index of key
-    var index = books.findIndex((book) => {
+    var myFunction = function(book) {
         return book.title === key;
-    });
-    console.log(key);
-    console.log(index);
+    };
+    var index = books.findIndex(myFunction);
     //delete array element at index of key
     return books.splice(index,1);
 };
-
-//working
-// var myVar = "The Great Gatsby";
-// var myFunction = function(book) {
-//     return book.title === myVar;
-// };
-// console.log(books.find(myFunction, myVar));
