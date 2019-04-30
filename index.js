1f3ea1de8f140a30453d577b36b1f47c7a819416
John Nguyen <john.nguyen3@seattlecentral.edu>
2019-04-30 00:30:35 +0000

    updated eslint rules and fixed 5 'no-var' warnings

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let handlebars =  require("express-handlebars");
 app.engine(".html", handlebars({extname: '.html'}));
 app.set("view engine", ".html");
 
-var books = require("./book.js");
+let books = require("./book.js");
 
 // send static file as response
 app.get('/', (req, res) => {
@@ -28,16 +28,16 @@ app.get('/about', (req, res) => {
 // POST method via form submission, form action leads to detail, 
 app.post('/detail', (req, res) => {
     //console.log(req.body); // display parsed form submission
-    var title = req.body.title;
-    var result = books.get(title);
+    let title = req.body.title;
+    let result = books.get(title);
     res.render('details', {title: title, result: result});
 });
 
 // GET method via details 
 app.get('/delete', (req,res) => {
     //console.log(req.query); // display parsed querystring object
-    var title = req.query.title
-    var result = books.delete(title);
+    let title = req.query.title
+    let result = books.delete(title);
     res.render('delete', {title: title, result: result, length: books.getAll().length});
 });
 
