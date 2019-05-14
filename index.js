db499526df3922bc75034d1f6351ddaea2211f90
John Nguyen <john.nguyen3@seattlecentral.edu>
2019-05-14 00:49:14 +0000

    Week 5 - Server Integration Cleaned up code, removed unnecessary comments.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.set('port', process.env.PORT || 3000); //used as app.get('port')
+app.set('port', process.env.PORT || 3000); // used as app.get('port')
 app.use(express.static(__dirname + '/public')); // set location for static files
 app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
 
@@ -12,33 +12,24 @@ app.engine(".html", handlebars({extname: '.html'}));
 app.set("view engine", ".html");
 
 let Book = require("./models/book-model");
-//let books = require("./book.js");
-//console.log(books);
-/*
-{ getAll: [Function],
-  get: [Function],
-  delete: [Function],
-  add: [Function] }
-*/
-// send static file as response
+
+// renders home page with inventory
 app.get('/', (req, res) => {
     res.type('text/html');
-    //res.sendFile(__dirname + '/public/home.html');
     
-    // insert back a single record (test: 'dune' in delete case)
+    // uncomment to update/insert back a single record
+    // testing: 'dune' in delete case
+    /*
     var newBook = {'title':'dune', 'author':'frank herbert', 'pubdate': 1963 }
     Book.updateOne({'title':'dune'}, newBook, {upsert:true}, (err, result) => {
       if (err) return next(err);
       // console.log(result);
-      // other code here
     });
     
+    */
     // return all records
     Book.find({}, (err, items) => {
         if (err) return next(err);
-        // console.log(items.length);
-        // console.log(items);
-        // other code here
         res.render('../public/home', {items: items});
     });
 });
@@ -51,57 +42,38 @@ app.get('/about', (req, res) => {
 
 // POST method via form submission, form action leads to detail
 app.post('/detail', (req, res) => {
-    // console.log(req.body); // display parsed form submission
     let title = req.body.title;
-    // let result = books.get(title);
-    // res.render('details', {title: title, result: result});
     
     // return a single record
     Book.findOne({'title': title}, (err, item) => {
         if (err) return next(err);
-        // console.log(item);
-        // { _id: 5cc7a69e1c9d44000068f389,
-        //   title: 'Moby Dick',
-        //   author: 'Herman Melville',
-        //   pubdate: 1851-01-01T00:00:00.000Z }
-        // other code here
         res.render('details', {result: item});
     });
 });
 
 // GET method via home
 app.get('/detail', (req, res) => {
-    // console.log(req.query); // display parsed querystring object
     let title = req.query.title;
     
     // return a single record
     Book.findOne({'title': title}, (err, item) => {
         if (err) return next(err);
-        // console.log(item);
-        // other code here
         res.render('details', {result: item});
     });
 });
 
 // GET method via details 
 app.get('/delete', (req,res) => {
-    //console.log(req.query); // display parsed querystring object
     let title = req.query.title;
-    // let result = books.delete(title);
-    // res.render('delete', {title: title, result: result, length: books.getAll().length});
-    var number = 1;
+    let number;
+    
     Book.deleteOne({'title': title}, (err, item) => {
         if (err) return next(err);
-        //console.log('num first: '+number); //1
-        Book.countDocuments({}, (err , count) => {
+        Book.countDocuments({}, (err, count) => {
             if (err) return next(err);
             number = count;
-            //console.log('num inside: '+number); //5
             res.render('delete', {title: title, result: item, length: number});
         });
-        //console.log('num outside: '+number); //still 1???
-        //scope issues, something about block scopes
-        //res.render('delete', {title: title, result: item, length: number});
     });
     
 });
@@ -115,5 +87,5 @@ app.use( (req,res) => {
 
 // start server listening for connections
 app.listen(process.env.PORT, process.env.IP, () => {
-    //console.log('Express started'); 
+    // console.log('Express started'); 
 });
--- models/book-model.js
+++ models/book-model.js
@@ -10,22 +10,22 @@ const credentials = require('../secret/credentials.js');
 
 mongoose.connect(credentials.db.connectionString, { dbName: credentials.db.name, useNewUrlParser: true }); //
 
-//Get the default connection
+// Get the default connection
 let db = mongoose.connection;
 
-//Bind connection to error event (to get notification of connection errors)
+// Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 db.on('open', () => {
-  console.log('Mongoose connected.');
+    console.log('Mongoose connected.');
 });
 
 // define Book model in JSON key/value pairs
 // values indicate the data type of each key
 const mySchema = mongoose.Schema({
- title: { type: String, required: true },
- author: String,
- pubdate: Date
+    title: { type: String, required: true },
+    author: String,
+    pubdate: Date
 }); 
 
 module.exports = mongoose.model('Book', mySchema);
